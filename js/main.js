e5f30e8c1d5ce1ecf83f98c7bc95276e6531d814
screen <bao.future94@gmail.com>
2020-11-19 02:16:38 -0800

    v3

--- js/main.js
+++ js/main.js
@@ -1,26 +1,36 @@
 App = {};
 
 (function ($) {
+
+    App.checkWindowSize = () => {
+        var width = $(window).width(),
+            new_class = width > 1199 ? "" : "carousel-company owl-carousel";
+
+        $(".products").removeClass("carousel-company owl-carousel owl-loaded owl-drag").addClass(new_class);
+        
+    };
+
+    
     // Load First
     $('.carousel').owlCarousel({
         loop: true,
         margin: 10,
         responsiveClass: true,
-        dots:false,
+        dots: false,
         responsive: {
             0: {
                 items: 2,
-                navText: ["<img src='../img/arrow-left.png'>","<img src='../img/arrow-right.png'>"],
+                navText: ["<img src='../img/arrow-left.png'>", "<img src='../img/arrow-right.png'>"],
                 nav: true
             },
             768: {
                 items: 2,
-                navText: ["<img src='../img/arrow-left.png'>","<img src='../img/arrow-right.png'>"],
+                navText: ["<img src='../img/arrow-left.png'>", "<img src='../img/arrow-right.png'>"],
                 nav: true
             },
             992: {
                 items: 1,
-                navText: ["<img src='../img/arrow-left.png'>","<img src='../img/arrow-right.png'>"],
+                navText: ["<img src='../img/arrow-left.png'>", "<img src='../img/arrow-right.png'>"],
                 nav: true,
                 margin: 0
             }
@@ -47,7 +57,37 @@ App = {};
             }
         }
     });
+   
+    // When resize screen
+    $(window).resize(function () {
+        App.checkWindowSize();
+    });
 
+     // Run first
+     $(document).ready(function () { 
+        App.checkWindowSize();
+
+        $('.carousel-company').owlCarousel({
+            loop: true,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 2,
+                    nav: false
+                },
+                768: {
+                    items: 3,
+                    nav: false
+                },
+                1024: {
+                    items: 3,
+                    nav: false
+                }
+            }
+        });
+     });
+  
 })(jQuery);
 
 
